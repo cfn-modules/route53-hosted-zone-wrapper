8aa37437a6b3a8ca89878c26b15f08aeff3fdba9
Andreas Wittig <andreas@widdix.de>
2024-12-13 08:21:23 +0100

    Fixing custom resource

--- lambda-src/custom-resource.js
+++ lambda-src/custom-resource.js
@@ -1,16 +1,58 @@
-const response = require('cfn-response');
 const { Route53Client, GetHostedZoneCommand } = require('@aws-sdk/client-route-53');
 const route53 = new Route53Client({apiVersion: '2012-11-05'});
 
+async function cfnCustomResourceSuccess(event, physicalResourceId, optionalData) {
+  const response = await fetch(event.ResponseURL, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      Status: 'SUCCESS',
+      PhysicalResourceId: physicalResourceId,
+      StackId: event.StackId,
+      RequestId: event.RequestId,
+      LogicalResourceId: event.LogicalResourceId,
+      ...(optionalData !== undefined && {Data: optionalData})
+    })
+  });
+  if (response.status !== 200) {
+    console.log('response status', response.status);
+    console.log('response', await response.text());
+    throw new Error('unexpected status code');
+  }
+}
+
+async function cfnCustomResourceFailed(event, physicalResourceId, optionalReason) {
+  const response = await fetch(event.ResponseURL, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      Status: 'FAILED',
+      ...(optionalReason !== undefined && {Reason: optionalReason}),
+      PhysicalResourceId: (physicalResourceId === undefined || physicalResourceId === null) ? event.LogicalResourceId : physicalResourceId, // physicalResourceId might not be available if create fails 
+      StackId: event.StackId,
+      RequestId: event.RequestId,
+      LogicalResourceId: event.LogicalResourceId
+    })
+  });
+  if (response.status !== 200) {
+    console.log('response status', response.status);
+    console.log('response', await response.text());
+    throw new Error('unexpected status code');
+  }
+}
 
 exports.handler = (event, context, cb) => {
   console.log(JSON.stringify(event));
   const error = (err) => {
     console.log(JSON.stringify(err));
-    response.send(event, context, response.FAILED);
+    cfnCustomResourceFailed(event);
   };
   if (event.RequestType === 'Delete') {
-    response.send(event, context, response.SUCCESS);
+    cfnCustomResourceSuccess(event, event.ResourceProperties.HostedZoneId);
   } else if (event.RequestType === 'Create' || event.RequestType === 'Update') {
     route53.send(new GetHostedZoneCommand({
       Id: event.ResourceProperties.HostedZoneId
@@ -18,9 +60,9 @@ exports.handler = (event, context, cb) => {
       if (err) {
         error(err);
       } else {
-        response.send(event, context, response.SUCCESS, {
+        cfnCustomResourceSuccess(event, event.ResourceProperties.HostedZoneId, {
           Name: data.HostedZone.Name.replace(/\.$/, '')
-        }, event.ResourceProperties.HostedZoneId);
+        });
       }
     });
   } else {
